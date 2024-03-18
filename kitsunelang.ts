150f8c81a4d6cbbcc78e924794cfa89d0d8b323a
Trin Wasinger <41750582+SteveBeeblebrox@users.noreply.github.com>
2024-03-17 20:13:31 -0600

    Update

--- kitsunelang.ts
+++ kitsunelang.ts
@@ -5,8 +5,11 @@ namespace KitsuneLang {
     export namespace Internals {}
     // @ts-expect-error
     export const LEVEL: number = KITSUNE_LANG_LEVEL;
-    ///#warning no version set
-    export const VERSION = null;
+
+    export const VERSION: Readonly<{major: number, minor: number, patch: number, metadata?: string, prerelease?: string, toString(): string}> = Object.freeze({
+        toString() {return `${VERSION.major}.${VERSION.minor}.${VERSION.patch}${VERSION.prerelease !== undefined ? `-${VERSION.prerelease}` : ''}${VERSION.metadata !== undefined ? `+${VERSION.metadata}` : ''}`},
+        major: 0, minor: 1, patch: 0, prerelease: 'alpha'
+    });
 }
 
 /// #if KITSUNE_LANG_LEVEL >= 0
